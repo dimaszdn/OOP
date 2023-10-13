66486ea84a22aaf8bef76cd7742d6d7ab78e53c6
Дмитрий Чепелев <dimaszdn@mail.ru>
2023-10-13 21:40:14 +0200

    completed

--- Task 2/inc/Array3D.h
+++ Task 2/inc/Array3D.h
@@ -9,100 +9,215 @@ private:
     int m_size = x * y * z;
     std::vector<T> m_v;
 public:
-    Array3D(const std::vector<T>& v)
+    Array3D()
     {
-        if (v.size() > m_size) //Дабы вектор сам не расширился
-            throw std::out_of_range("dimensions do not match!");
+        m_v.resize(m_size);
+    }
+
+    explicit Array3D(const std::vector<T>& v)
+    {
+        if (v.size() > m_size)
+            throw std::out_of_range("Dimensions do not match!");
         m_v = v;
     }
 
-    int getIndex(int i, int j, int k)
+    //return vec2d
+    std::vector<std::vector<T>> getValue0(int i) //0
     {
-        return (i + j * x) + (k * x * y);
-    }
-
-    //return 2d vec
-    std::vector<std::vector<T>> getValue0(int k)
-    {
-        std::vector<std::vector<T>> vec2d(x, std::vector<T>(y));
-        int begin = this->getIndex(0, 0, k);
-        int end = begin + (m_v.size() / z);
-        int row = 0, col = 0;
-        while (begin < end)
+        std::vector<std::vector<T>> vec2d;
+        std::vector<T> vec1d;
+        for (int a = 0; a < y; ++a)
         {
-            if (col >= y)
+            if (!vec1d.empty())
+                vec1d.clear();
+            for (int b = 0; b < z; ++b)
             {
-                ++row;
-                col = 0;
+                vec1d.push_back(this->getValue(i, a, b));
             }
-            vec2d[row][col] = m_v[begin];
-            ++col;
-            ++begin;
+            vec2d.push_back(vec1d);
         }
         return vec2d;
     }
 
-    //return 1d vec
-    std::vector<T> getValue1(int j) //think
+    //return vec2d
+    std::vector<std::vector<T>> getValue1(int j) //1
     {
-        std::vector<T> vec1d(y);
-        int begin = this->getIndex(0, j, 0);
-        int end = begin + y;
-        std::copy(m_v.begin() + begin, m_v.begin + end, vec1d.begin());
-//        int col = 0;
-//        while (begin < end)
-//        {
-//            vec1d[col] = m_v[begin];
-//            ++begin;
-//            ++col;
-//        }
+        std::vector<std::vector<T>> vec2d;
+        std::vector<T> vec1d;
+        for (int a = 0; a < x; ++a)
+        {
+            if (!vec1d.empty())
+                vec1d.clear();
+            for (int b = 0; b < z; ++b)
+            {
+                vec1d.push_back(this->getValue(a, j, b));
+            }
+            vec2d.push_back(vec1d);
+        }
+        return vec2d;
+    }
+
+    //return vec2d
+    std::vector<std::vector<T>> getValue2(int k) //1
+    {
+        std::vector<std::vector<T>> vec2d;
+        std::vector<T> vec1d;
+        for (int a = 0; a < x; ++a)
+        {
+            if (!vec1d.empty())
+                vec1d.clear();
+            for (int b = 0; b < y; ++b)
+            {
+                vec1d.push_back(this->getValue(a, b, k));
+            }
+            vec2d.push_back(vec1d);
+        }
+        return vec2d;
+    }
+
+    //return vec1d
+    std::vector<T> getValues01(int i, int j) //1 1
+    {
+        std::vector<T> vec1d;
+        for (int c = 0; c < z; ++c)
+            vec1d.push_back(this->getValue(i, j, c));
         return vec1d;
     }
 
-    int getX() { return x; }
+    //return vec1d
+    std::vector<T> getValues02(int i, int k) // 1 1
+    {
+        std::vector<T> vec1d;
+        for (int b = 0; b < y; ++b)
+            vec1d.push_back(this->getValue(i, b, k));
+        return vec1d;
+    }
 
-    int getY() { return y; }
+    //return vec1d
+    std::vector<T> getValues12(int j, int k) // 1 1
+    {
+        std::vector<T> vec1d;
+        for (int a = 0; a < x; ++a)
+            vec1d.push_back(this->getValue(a, j, k));
+        return vec1d;
+    }
 
-    int getZ() { return z; }
+    //set vec2d
+    void setValue0(const std::vector<std::vector<T>>& vec2d, int i) //1 {A} = {{0, 0, 0}, {0, 0}};  C = {{500, 600}, {700}};
+    {
+        for (int j = 0; j < y; ++j)
+        {
+            for (int k = 0; k < z; ++k)
+            {
+                if (j < vec2d.size() && k < vec2d[j].size())
+                    this->setValue(i, j, k, vec2d[j][k]);
+            }
+        }
+    }
+
+    //set vec2d
+    void setValue1(const std::vector<std::vector<T>>& vec2d, int j) //0 A
+    {
+        for (int i = 0; i < x; ++i)
+        {
+            for (int k = 0; k < z; ++k)
+            {
+                if (i < vec2d.size() && k < vec2d[i].size())
+                    this->setValue(i, j, k, vec2d[i][k]);
+            }
+        }
+    }
+
+    //set vec2d
+    void setValue2(const std::vector<std::vector<T>>& vec2d, int k) // C 2
+    {
+        for (int i = 0; i < x; ++i)
+        {
+            for (int j = 0; j < y; ++j)
+            {
+                if (i < vec2d.size() && j < vec2d[i].size())
+                    this->setValue(i, j, k, vec2d[i][j]);
+            }
+        }
+    }
+
+    //set vec1d
+    void setValue01(const std::vector<T>& vec1d, int i, int j) // B = {0, 0, 0, 0}; D = {500, 600, 700, 800, 900}; B 1 0
+    {
+        for (int k = 0; k < z; ++k)
+        {
+            if (k < vec1d.size())
+                this->setValue(i, j, k, vec1d[k]);
+        }
+    }
+
+    //set vec1d
+    void setValue02(const std::vector<T>& vec1d, int i, int k) // B 0 1
+    {
+        for (int j = 0; j < y; ++j)
+        {
+            if (j < vec1d.size())
+                this->setValue(i, j, k, vec1d[j]);
+        }
+    }
+
+    //set vec1d
+    void setValue12(const std::vector<T>& vec1d, int j, int k) // D 1 1
+    {
+        for (int i = 0; i < x; ++i)
+        {
+            if (i < vec1d.size())
+                this->setValue(i, j, k, vec1d[i]);
+        }
+    }
+
+    static Array3D* createFill(T value)
+    {
+        auto* object = new Array3D<T, x, y, z>();
+        object->fillVec(value);
+        return object;
+    }
+
+    void print()
+    {
+        bool flag;
+        for (int i = 0; i < m_v.size(); ++i)
+        {
+            flag = true;
+            if (i % (m_size / z) == 0 && i != 0)
+            {
+                std::cout << "| ";
+                flag = false;
+            }
+            if (flag && i % x == 0 && i != 0)
+            {
+                std::cout << ", ";
+            }
+            std::cout << m_v[i] << " ";
+        }
+
+        std::cout << "\n";
+    }
+
+private:
+    int getValue(int i, int j, int k)
+    {
+        int index = (m_size / x) * i + (m_size / (x * y)) * j + k;
+        if (index >= m_size)
+            throw std::out_of_range("Invalid index!");
+        return m_v[index];
+    }
+
+    void setValue(int i, int j, int k, T value)
+    {
+        int index = (m_size / x) * i + (m_size / (x * y)) * j + k;
+        if (index >= m_size)
+            throw std::out_of_range("Invalid index!");
+        m_v[index] = value;
+    }
+
+    void fillVec(T value)
+    {
+        std::fill(m_v.begin(), m_v.end(), value);
+    }
 };
-
-//template<typename T, int x = 0, int y = 0, int z = 0>
-//class Array3D
-//{
-//private:
-//    int m_cap = x * y * z;
-//    T m_mas[x * y * z];
-//
-//public:
-//    Array3D(const T(&mas)[x * y * z])
-//    {
-//        for (int i = 0; i < x * y * z; ++i)
-//        {
-//            m_mas[i] = mas[i];
-//        }
-//    }
-//
-//    T* GetValue0(int i)
-//    {
-//        T tmp[x * y];
-//        T* pointer = this->getValue(0, 0, i);
-//        std::copy(pointer, pointer + m_cap / z, tmp);
-//        return tmp;
-//    }
-//
-//    //GetValue1(int y)
-//
-//    //GetValue2(int z)
-//
-//    static void creatteFill()
-//    {
-//
-//    }
-//
-//private:
-//    T* getValue(int i, int j, int k)
-//    {
-//        return &m_mas[(i + j * x) + (k * x * y)];
-//    }
-//
-//};
--- Task 2/main.cpp
+++ Task 2/main.cpp
@@ -3,39 +3,111 @@
 #include<vector>
 
 template<typename T>
-void print_vec(std::vector<T>& v)
+void print_vec2d(const std::vector<std::vector<T>>& v)
 {
     for (int i = 0; i < v.size(); ++i)
     {
         for (int j = 0; j < v[i].size(); ++j)
             std::cout << v[i][j] << " ";
-        std::cout << "\n";
+        if (i != v.size() - 1)
+            std::cout << "| ";
     }
+    std::cout << "\n";
+}
+
+template<typename T>
+void print_vec1d(const std::vector<T>& v)
+{
+    for (int i = 0; i < v.size(); ++i)
+    {
+        std::cout << v[i] << " ";
+    }
+    std::cout << "\n";
 }
 
 int main()
 {
-//    // { { {1, 4}, {5, 6} },| { {2, 3}, {7, 8} } }
-////    Array3D<int, 2, 2, 2> a({1, 4, 5, 6, 2, 3, 7, 8});
-//    Array3D<int, 3, 2, 3> a({1, 4, 80, 5, 6, 2, 3, 0, 7, 8, 13, 12, 10, 11, 9, 76, 45});
-//    int* mas = a.GetValue0(1);
-//    for (int i = 0; i < 6; ++i)
-//        std::cout << mas[i] << " ";
-//
-////    std::cout << N;
     try
     {
-        Array3D<int, 3, 2, 3> a({1, 4, 80, 5, 6, 2, 3, 0, 7, 8, 13, 12, 10, 11, 9, 76, 45});
-        Array3D<int, 3, 2, 3> a1({});
-//        std::vector<std::vector<int>> v = a.getValue0(0);
-//        print_vec(v);
-        std::vector<std::vector<int>> v = {{1, 2}, {3}};
-        std::vector<std::vector<int>> v1 = a1.getValue0(0);
-//        std::vector<int> v2 = a.getValue1(1);
-        print_vec(v1);
+        Array3D<int, 3, 2, 3> a1({1, 4, 80, 5, 6, 2, 3, 0, 7, 8, 13, 12, 10, 11, 9, 76, 45, 30});
+        Array3D<int, 2, 2, 2> a2({1, 4, 5, 6, 2, 3, 7, 8});
 
+        std::cout << "Getters" << "\n";
+
+        std::cout << "[Array3D]: ";
+        a1.print();
+        std::vector<std::vector<int>> v1 = a1.getValue0(0);
+        std::cout << "[getValue0]: ";
+        print_vec2d(v1);
+
+        std::vector<std::vector<int>> v2 = a1.getValue1(1);
+        std::cout << "[getValue1]: ";
+        print_vec2d(v2);
+
+        std::vector<std::vector<int>> v3 = a1.getValue2(1);
+        std::cout << "[getValue2]: ";
+        print_vec2d(v3);
+
+        std::cout << "\n";
+
+        std::vector<int> v4 = a1.getValues01(1, 1);
+        std::cout << "[getValues01]: ";
+        print_vec1d(v4);
+
+        std::vector<int> v5 = a1.getValues02(1, 1);
+        std::cout << "[getValues02]: ";
+        print_vec1d(v5);
+
+        std::vector<int> v6 = a1.getValues12(1, 1);
+        std::cout << "[getValues12]: ";
+        print_vec1d(v6);
+
+        std::cout << "\n";
+        //####################################
+
+        std::cout << "Setters" << "\n";
+
+        std::vector<std::vector<int>> A = {{0, 0, 0}, {0, 0}};
+        std::vector<std::vector<int>> C = {{500, 600}, {700}};
+
+        std::cout << "[Array3D]: "; a1.print();
+
+        a1.setValue0(A, 1);
+        std::cout << "[setValue0]: ";
+        a1.print();
+
+        a1.setValue1(A, 0);
+        std::cout << "[setValue1]: ";
+        a1.print();
+
+        a1.setValue2(C, 2);
+        std::cout << "[setValue2]: ";
+        a1.print();
+
+        std::cout << "\n";
+
+        std::vector<int> B = {0, 0, 0, 0};
+        std::vector<int> D = {500, 600, 700, 800, 900};
+
+        std::cout << "[Array3D]: "; a2.print();
+
+        a2.setValue01(B, 1, 0);
+        std::cout << "[setValue01]: ";
+        a2.print();
+
+        a2.setValue02(B, 0, 1);
+        std::cout << "[setValue02]: ";
+        a2.print();
+
+        a2.setValue12(D, 1, 1);
+        std::cout << "[setValue12]: ";
+        a2.print();
+
+        auto* a3 = Array3D<int, 4, 3, 2>::createFill(5);
+        a3->print();
+        delete a3;
     }
-    catch (std::out_of_range& ex)
+    catch (const std::out_of_range& ex)
     {
         std::cout << ex.what() << "\n";
     }
